47fd7b5c2e0ca4b4c42eac4409ace51a5b562077
rodrigo-f7 <hardcoreplays23@gmail.com>
2023-10-22 14:41:42 -0300

    Mensagem a ser passada

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,8 @@
 public class HelloWorld{
     public static void main(String[] args){
+        int x = 2;
         System.out.print("Hello, World. ");
         System.out.println("This is my program!");
+        System.out.println("x = " + x);
     }
 }
